86f7da62597716e999ff592ac51aa032f16103d1
mjJang94 <alswhddl10@naver.com>
2025-07-30 10:04:47 +0900

    검색 결과 리스트 레이아웃 정리

--- ui/src/main/java/com/mj/ui/screen/SearchScreen.kt
+++ ui/src/main/java/com/mj/ui/screen/SearchScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -27,11 +28,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.paging.LoadState
 import androidx.paging.PagingData
@@ -40,10 +46,12 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import coil3.request.crossfade
+import coil3.request.fallback
 import com.mj.presentation.search.model.BookModel
 import com.mj.presentation.search.model.ImageLinksModel
 import com.mj.presentation.search.model.VolumeModel
 import com.mj.presentation.search.search.BookSearchViewModel
+import com.mj.ui.R
 import com.mj.ui.theme.PreviewTheme
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
@@ -152,17 +160,16 @@ private fun SearchScreenContent(
 
 @Composable
 private fun BookItem(data: VolumeModel) {
-    Column(
+    Row(
         modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start,
     ) {
-
-
         AsyncImage(
             modifier = Modifier.size(100.dp),
             model = ImageRequest.Builder(LocalContext.current)
                 .data(data.imageLinks.smallThumbnail)
+                .fallback(R.drawable.baseline_image_not_supported_24)
                 .crossfade(true)
                 .build(),
             contentDescription = "Book Thumbnail",
@@ -170,7 +177,15 @@ private fun BookItem(data: VolumeModel) {
                 Log.e("AsyncImage", "이미지 로딩 실패: ${error.result.throwable}")
             }
         )
-        Text(data.title)
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start,
+        ) {
+            Text(text = data.title, fontSize = 16.sp, maxLines = 2, overflow = TextOverflow.Ellipsis, color = Color.Black, fontWeight = FontWeight.Bold)
+            Text(text = data.authors.firstOrNull() ?: "정보 없음", fontSize = 14.sp, maxLines = 1, overflow = TextOverflow.Ellipsis, color = Color.Black)
+            Text(text = data.publishedDate, fontSize = 12.sp, maxLines = 1, overflow = TextOverflow.Ellipsis, color = Color.Black)
+        }
     }
 }
 
@@ -220,6 +235,26 @@ private fun QueryTextField(
             }
         }
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BookItemPreview() {
+    PreviewTheme {
+
+        val info = VolumeModel(
+            title = "미리보기 책 제목",
+            authors = listOf("작가 1", "작가 2"),
+            publishedDate = "2023-01-01",
+            printType = "book",
+            infoLink = "",
+            imageLinks = ImageLinksModel(
+                smallThumbnail = "",
+                thumbnail = "",
+            )
+        )
+        BookItem(info)
+    }
 }
 
 @Preview(showBackground = true)
