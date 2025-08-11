f56894970583255d46fb5848dc615adcfe68b04e
mjJang94 <alswhddl10@naver.com>
2025-08-11 22:50:39 +0900

    import문 정리

--- ui/src/main/java/com/mj/ui/NavGraph.kt
+++ ui/src/main/java/com/mj/ui/NavGraph.kt
@@ -1,16 +1,11 @@
 package com.mj.ui
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
-import androidx.paging.compose.LazyPagingItems
-import androidx.paging.compose.collectAsLazyPagingItems
-import com.mj.presentation.search.model.BookModel
 import com.mj.presentation.search.search.BookSearchViewModel
 import com.mj.ui.screen.SearchScreen
 
